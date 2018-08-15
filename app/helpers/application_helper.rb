0d19f382ea9572c1f189c1dcbe1dcaadc74074ed
zhao <njzhaoguobin@126.com>
2018-08-15 15:49:59 +0800

    Add Admin::ApplicationController.
    Signed-off-by: zhao <njzhaoguobin@126.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def active_class(active_controller, active_action = nil)
+    if controller_name == active_controller
+      'active' unless active_action.present? && action_name != active_action
+    end
+  end
+  
 end
